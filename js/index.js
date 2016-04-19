781a5af2431b41a24106af84fc9a355315c431c0
wearesober <thesobrietycollective@gmail.com>
2016-04-18 20:34:47 -0400

    added JS

--- js/index.js
+++ js/index.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
 
+//When user inputs city name and clicks submit, show pix for that city.
+
+$('#submit-btn').click(showCityPix);
+event.preventDefault();
 
 
+function showCityPix() {
+	
+ console.log(is this working);
+
+ var = "var city"
+
+ if ('var city' == "NYC") {
+ 	$('#city-type').val(NYC);
+ } else if ('var city' =="New York City");{
+ 	$('#city-type').val(New York City); {
+ } else if ('var city' == "New York"); {
+ 	$('#city-type').val(New York);
+ 	}
+ } 
+
+ }
+
+	a = $('').addClass('.nyc');
+	b = $().addClass('.sf');
+	c = $().addClass('la');
+	d = $().addClass('.austin');
+	e = $().addClass('.sydney');
+
+	
+}
+
+ 
 
 
 
 });
+
+
